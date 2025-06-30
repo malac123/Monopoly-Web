51d92449b671ec514de911273f607736a0a56fc4
SplatTB <schlankerdicker@gmail.com>
2025-06-30 09:02:10 +0200

    Liam ivano print on site

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+first_launch = True 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    global first_launch
+    
+    if first_launch:
+        user = "Ivano"
+        first_launch = False
+    else:
+        user = "Liam"
+        first_launch = True
+
+        
+    return render_template('index.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
