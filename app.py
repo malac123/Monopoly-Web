312fd896c70fbef74de72f1f2171f05a6211740c
SplatTB <schlankerdicker@gmail.com>
2025-06-30 09:55:18 +0200

    zwischen 2 html seiten navigiern und infos speichern

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-user_age = 25
-user_name = "Unknown"
+user_name = "Unbekannt"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global user_age, user_name
-
+    global user_name
     if request.method == 'POST':
-        user_age = int(request.form.get('age', user_age))
-        user_name = request.form.get('name', user_name)
+        user_name = request.form['name']
+    return render_template('index.html', user_name=user_name)
 
-    return render_template('index.html', user_age=user_age, user_name=user_name)
-
+@app.route('/mainpage')
+def mainpage():
+    global user_name
+    if request.method == 'POST':
+        user_name = request.form['name']
+    return render_template('mainpage.html', user_name=user_name)
 if __name__ == '__main__':
     app.run(debug=True)
