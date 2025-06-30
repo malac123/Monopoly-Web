bff01d23ebd6169b6a5bcd6fd262c798c4efdd00
SplatTB <schlankerdicker@gmail.com>
2025-06-30 08:37:51 +0200

    flask setup complete

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    user = {'name': 'Max', 'age': 25}
+    return render_template('index.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+index()
